79e07e6534f4177955e4e1867e8ecde4ea4328a9
Bart-15 <brat.tabusao@gmail.com>
2025-06-29 18:48:12 +0800

    feat: add polly for text to speech

---
+++ cdk/lambda/utils/speak.ts
@@ -0,0 +1,28 @@
+import { PollyClient, SynthesizeSpeechCommand } from "@aws-sdk/client-polly";
+import { Readable } from "stream";
+import { Buffer } from "buffer";
+
+const polly = new PollyClient({});
+
+/**
+ * Synthesizes speech from text using AWS Polly.
+ *
+ * @param {string} text the text to synthesize
+ * @returns {Promise<Buffer>} a promise that resolves to the audio as an MP3 buffer
+ */
+export const synthesizeSpeech = async (text: string): Promise<Buffer> => {
+  const { AudioStream } = await polly.send(
+    new SynthesizeSpeechCommand({
+      OutputFormat: "mp3",
+      Text: text,
+      VoiceId: "Joanna", // TODO: Parametarized
+    })
+  );
+
+  const audioChunks: Buffer[] = [];
+  for await (const chunk of AudioStream as Readable) {
+    audioChunks.push(chunk);
+  }
+
+  return Buffer.concat(audioChunks);
+};
